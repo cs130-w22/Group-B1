3fe155f4ff6b12bcdc876f0336e090c646d40779
Deborah Xintong Liu <xtliudeborah@gmail.com>
2022-01-26 21:37:47 -0800

    Bind authorization controller and service to the container

--- backend/types.ts
+++ backend/types.ts
@@ -2,6 +2,8 @@ const TYPES = {
   RoommateRepository: Symbol("RoommateRepository"),
   RoommateService: Symbol("RoommateService"),
   Controller: Symbol("Controller"),
+
+  AuthorizationService: Symbol("AuthorizationService"),
 };
 
 export default TYPES;
