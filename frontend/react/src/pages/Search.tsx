9bf9074ae1d9b6fc87cf65f766cf75b75ee1558b
Ssirrikh <nadisasmito033@g.ucla.edu>
2022-01-28 12:11:21 -0800

    update search page to fully typescript and apply new color scheme

--- frontend/react/src/pages/Search.tsx
+++ frontend/react/src/pages/Search.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 
 import './Search.css';
 import '../shared/roommateProfile.ts';
@@ -61,6 +62,7 @@ function randomEnum<T>(anEnum: T): T[keyof T] {
 const firstnames:string[] = ['John', 'Joe', 'Katie', 'Lauren', 'Mariana', 'Katniss', 'Peter', 'Tyler', 'Tara', 'Ezekiel'];
 const lastnames:string[] = ['Smith', 'Cooper', 'Westfield', 'Everdeen', 'Rennel', 'Miller', 'Steinbech', 'Reinhardt'];
 const personalities:PersonalityTrait[] = ['introvert', 'extrovert', 'sensor', 'intuitive', 'thinker', 'feeler', 'judger', 'perceiver'];
+const areas:Area[] = ['Austin','Los Angeles','Miami','New York','San Francisco','Seattle'];
 var profiles:RoommateProfile[] = [];
 for (let i = 0; i < 30; i++) {
   let profile:RoommateProfile = {
@@ -68,17 +70,19 @@ for (let i = 0; i < 30; i++) {
     lastName: lastnames[randInt(0,lastnames.length-1)],
     email: 'example@email.com',
     //area: randomEnum(Areas),
-    area: 'Los Angeles',
+    area: areas[randInt(0,areas.length-1)],
     bio: '[about me]',
     hobbies: [],
     personality: [],
-    additionalInfo: '[more info]'
+    additionalInfo: '[additional info]'
   };
   for (let i = 0, n = randInt(2,5); i < n; i++) {
     profile.personality.push(personalities[randInt(0,personalities.length-1)])
   }
   profiles.push(profile);
 }
+var viewedProfile:RoommateProfile = profiles[0];
+var viewedId:number = 0;
 
 // fetch data
 function getProfiles() {
@@ -86,6 +90,11 @@ function getProfiles() {
 }
 
 // components
+const useProfile = (newId) => {
+  const [id, setId] = useState(0);
+  setId(newId);
+  return [id, setId];
+}
 const UserProfilePanel: React.FC = () => {
   return (
     <div className='user-profile-panel'/>
@@ -93,15 +102,19 @@ const UserProfilePanel: React.FC = () => {
 }
 const RoommateSelectionPanel: React.FC = () => {
   const profiles = getProfiles();
+  // const [id, setId] = useProfile(0);
+  const update = (newId) => {console.log('showing user ' + newId); viewedId = newId;}
   return (
     <div className='roommate-selection-panel'>
+      <p className='recommendation-text'>Recommendations</p>
+      <div className="miniViewFade"/>
       <div className="roommate-list-panel">
-        {profiles.map((profile) =>
-           <div className="miniProfile">
-             <div class="miniProfilePicture"></div>
-             <div class="miniProfilePreference"></div>
-             <p class="miniProfileName">{profile.firstName} {profile.lastName}</p>
-             <p class="miniProfileText">{profile.personality.join(', ')}</p>
+        {profiles.map((profile, newId) =>
+           <div className="miniProfile" onClick={()=>{update(newId)}}>
+             <div className="miniProfilePicture"></div>
+             <div className="miniProfilePreference"></div>
+             <p className="miniProfileName">{profile.firstName} {profile.lastName}</p>
+             <p className="miniProfileText">{profile.personality.join(', ')}</p>
            </div>
         )}
       </div>
@@ -109,10 +122,46 @@ const RoommateSelectionPanel: React.FC = () => {
   )
 }
 const ViewedProfilePanel: React.FC = () => {
+  // const [id, setId] = useState(0);
+  const [id, setId] = useState(0);
+  const update = () => {
+    // setId(prev => {
+    //   console.log('update to ' + ((prev+1)%profiles.length));
+    //   return (prev+1)%profiles.length;
+    // });
+    // console.log(id);
+    // setId((id+1)%profiles.length);
+    setId(viewedId);
+  }
   return (
-    <div className='viewed-profile-panel'/>
+    <div className='viewed-profile-panel' onClick={update}>
+      <div className="fullProfilePicture"/>
+      <div className="fullProfilePreference"/>
+      <p className="fullProfileName">{profiles[id].firstName} {profiles[id].lastName}</p>
+      <p className="fullProfileText">{profiles[id].personality.join(', ')}</p>
+      <p className="email">{profiles[id].email}</p>
+      <div className="fullProfileBio">
+        <p>I am from <b>{profiles[id].area}</b>!</p>
+        <p>{profiles[id].bio}</p>
+        <p>{profiles[id].additionalInfo}</p>
+      </div>
+    </div>
   )
 }
+
+
+// <div className="fullProfilePicture" onClick={update}/>
+
+
+// contentStr += '<div id="fullProfilePicture"></div>';
+// contentStr += '<div id="fullProfilePreference"></div>';
+// contentStr += '<p id="fullProfileName">' + profiles[profileIndex].firstname + ' '  + profiles[profileIndex].lastname + '</p>';
+// contentStr += '<p id="fullProfileText">' + profiles[profileIndex].personality + '</p>';
+// contentStr += '<div id="personalityPreferences">[personality: self-desc + roommate prefs]</div>';
+// contentStr += '<div id="housingPreferences">[housing: prefs + current residence if applicable]</div>';
+// contentStr += '<p id="email">example@email.com</p>';
+
+
 
 // page
 const Search: React.FC = () => {
