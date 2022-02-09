daf0d8d07fa2af1e85a696b4b1451b1ee33d901e
Jeremy Louie <jeremysf74@yahoo.com>
2022-02-08 22:47:57 -0800

    Small overhaul on login UI

--- frontend/react/src/pages/Login.tsx
+++ frontend/react/src/pages/Login.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './Login.css';
 import * as Unicons from '@iconscout/react-unicons';
 
-var pencil_icon_size = 47.5
+var people1 = require('../resources/people1.png')
+// var pencil_icon_size = 35
 
 const Login: React.FC = () => {
   return (
     <div className="center">
       <div className="login_banner">
-        <div className="login_logo">
-          <Unicons.UilPen size={pencil_icon_size} className="home"/>
+        <img src={people1} className="people" alt="people" /> 
+        <div className="login_region">
+          <div className="instructions">
+            <p className="sentence1">Sign in</p>
+            <p className="sentence2">to find your new roommates</p>
+          </div>
+          <hr></hr>
+          <input type="text" placeholder=" Username"/>
+          <input type="password" placeholder=" Password" />
+          <div className="login_button">
+            <p>Login</p>
+          </div>
         </div>
-        <p>Login</p>
       </div>
     </div>
   )
