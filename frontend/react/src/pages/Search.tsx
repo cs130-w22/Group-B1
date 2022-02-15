655feabb688998601e91f1c249132b5d4860a50f
Ssirrikh <nadisasmito033@g.ucla.edu>
2022-02-14 18:31:52 -0800

    add support for backend integration

--- frontend/react/src/pages/Search.tsx
+++ frontend/react/src/pages/Search.tsx
@@ -48,6 +48,10 @@ interface RoommateProfile {
   personality: PersonalityTrait[];
   additionalInfo: string;
 }
+interface Roommate {
+  username: string;
+  profile: RoommateProfile;
+}
 
 // stub data
 function randInt(min:number, max:number): number { return Math.floor(Math.random() * (max - min + 1)) + min; }
@@ -71,10 +75,10 @@ for (let i = 0; i < 30; i++) {
     email: 'example@email.com',
     //area: randomEnum(Areas),
     area: areas[randInt(0,areas.length-1)],
-    bio: '[about me]',
+    bio: '[about me] Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     hobbies: [],
     personality: [],
-    additionalInfo: '[additional info]'
+    additionalInfo: '[additional info] Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   };
   profile.email = profile.firstName + ' ' + profile.lastName + '@' + (Math.random()<0.5?'gmail':'yahoo') + '.com';
   for (let i = 0, n = randInt(2,5); i < n; i++) {
@@ -82,12 +86,106 @@ for (let i = 0; i < 30; i++) {
   }
   profiles.push(profile);
 }
+var roommates:Roommate[] = [];
+for (let profile of profiles) {
+  let roommate:Roommate = {
+    username: profile.firstName + Math.round(Math.random()*1000),
+    profile: profile
+  };
+  roommates.push(roommate);
+}
 var viewedProfile:RoommateProfile = profiles[0];
 var viewedId:number = 0;
 
+window['connectDev'] = {
+  username: 'testuser',
+  password: 'testpass',
+  profile: {
+    firstName: 'Katniss',
+    lastName: 'Everdeen',
+    email: 'survivor@gmail.com',
+    area: 'Los Angeles',
+    bio: '[test bio]',
+    hobbies: [],
+    personality: [],
+    additionalInfo: '[test info]'
+  },
+
+  roommates: roommates,
+
+  testGet: function(): void {
+    console.log('attempting get...');
+    var rootUrl = 'http://localhost:5000';
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        console.log(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", rootUrl, true); // true for async
+    xmlHttp.send(null);
+  }
+};
+
 // fetch data
+var authToken = ''; // eventually stored in cookie
+var rootUrl = 'http://localhost:5000';
 function getProfiles() {
   return profiles;
+}
+function getAllRoommates(callback:(string)=>void): void {
+  console.log('looking up roommates...');
+
+  // callback(roommates);
+  // return;
+
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", rootUrl+'/roommate/', true); // true for async
+  xmlHttp.setRequestHeader('authorization', authToken);
+  xmlHttp.send(null);
+}
+function getRoommate(username:string, callback:(string)=>void): void {
+  console.log('looking up profile for '+username+'...');
+
+  // for (let roommate of roommates) {
+  //   if (username == roommate.username) {
+  //     callback(roommate);
+  //     return;
+  //   }
+  // }
+  // return;
+
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", rootUrl+'/roommate/'+username, true); // true for async
+  xmlHttp.setRequestHeader('authorization', authToken);
+  xmlHttp.send(null);
+}
+function getFilteredRoommates(filter, callback:(string)=>void): void {
+  console.log('looking up roommates that match filter...');
+
+  let filterStr = '';
+  for (let property of filter) {
+    filterStr += (filterStr==''?'?':'&') + property + '=' + filter[property];
+  }
+
+  // callback(roommates);
+  // return;
+
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", rootUrl+'/roommate/'+filterStr, true); // true for async
+  xmlHttp.setRequestHeader('authorization', authToken);
+  xmlHttp.send(null);
 }
 
 // components
@@ -105,6 +203,7 @@ const UserProfilePanel: React.FC = () => {
     </div>
   )
 }
+const profilePicM = {backgroundImage: 'url(../resources/edit.png)'};
 const RoommateSelectionPanel: React.FC = () => {
   const profiles = getProfiles();
   // const [id, setId] = useProfile(0);
@@ -116,7 +215,7 @@ const RoommateSelectionPanel: React.FC = () => {
       <div className="roommate-list-panel">
         {profiles.map((profile, newId) =>
            <div className="miniProfile" onClick={()=>{update(newId)}}>
-             <div className="miniProfilePicture"></div>
+             <div className="miniProfilePicture" style={profilePicM}></div>
              <div className="miniProfilePreference"></div>
              <p className="miniProfileName">{profile.firstName} {profile.lastName}</p>
              <p className="miniProfileText">{profile.personality.join(', ')}</p>
