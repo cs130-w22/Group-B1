2ad7560e9be7f08ad41b519335129e669e08aae3
Ssirrikh <nadisasmito033@g.ucla.edu>
2022-02-18 12:55:08 -0800

    improve login

--- frontend/react/src/pages/Login.tsx
+++ frontend/react/src/pages/Login.tsx
@@ -1,46 +1,42 @@
 
 import React from 'react';
+import {useState} from 'react';
 import './Login.css';
 import * as Unicons from '@iconscout/react-unicons';
 
 var people1 = require('../resources/people1.png')
 
-function tryLogin(): void {
-  // let un = document.getElementById('username').value || 'testuser';
-  // let pw = document.getElementById('password').value || 'testpass';
-  // window['connectDev'].user.username = un;
-  // window['connectDev'].user.password = pw;
-
-  var rootUrl = window['connectDev'].rootUrl;
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() { 
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      var response = JSON.parse(xmlHttp.responseText);
-      window['connectDev'].authToken = response.accessToken;
-      console.log('login success');
-      console.log(xmlHttp.responseText);
-      window.location.pathname = '/search';
-    }
-  }
-  xmlHttp.open("POST", rootUrl+'/roommate/login', true); // true for async
-  xmlHttp.setRequestHeader('Content-Type', 'application/json');
-  xmlHttp.send(JSON.stringify({
-    username: window['connectDev'].user.username,
-    password: window['connectDev'].user.password
-  }));
-}
-
 const Login: React.FC = () => {
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const handleUsernameChange = (event) => {
     window['connectDev'].user.username = event.target.value;
+    setUsername(event.target.value);
   };
   const handlePasswordChange = (event) => {
     window['connectDev'].user.password = event.target.value;
+    setPassword(event.target.value);
   };
   const handleSubmit = (event) => {
-    tryLogin();
+    var rootUrl = window['connectDev'].rootUrl;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        var response = JSON.parse(xmlHttp.responseText);
+        window['connectDev'].authToken = response.accessToken;
+        console.log('login success');
+        console.log(xmlHttp.responseText);
+        window.location.pathname = '/search';
+      } else {
+        console.log('login failed');
+      }
+    }
+    xmlHttp.open("POST", rootUrl+'/roommate/login', true); // true for async
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.send(JSON.stringify({
+      username: username,
+      password: password
+    }));
   }
   return (
     <div className="login-center">
