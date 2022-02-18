8883062e4792f68de95f974d6fdcbc80dc53220b
Andrew Chang <39173193+archang19@users.noreply.github.com>
2022-02-18 13:28:01 -0800

    Login user (#64)
    Co-authored-by: Ssirrikh <nadisasmito033@g.ucla.edu>

--- frontend/react/src/pages/Login.tsx
+++ frontend/react/src/pages/Login.tsx
@@ -1,11 +1,42 @@
 
 import React from 'react';
+import {useState} from 'react';
 import './Login.css';
-import * as Unicons from '@iconscout/react-unicons';
 
-var people1 = require('../resources/people1.png')
+const people1 = require('../resources/people1.png')
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleUsernameChange = (event) => {
+    window['connectDev'].user.username = event.target.value;
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    window['connectDev'].user.password = event.target.value;
+    setPassword(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    const rootUrl = window['connectDev'].rootUrl;
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        const response = JSON.parse(xmlHttp.responseText);
+        window['connectDev'].authToken = response.accessToken;
+        console.log('login success');
+        console.log(xmlHttp.responseText);
+        window.location.pathname = '/search';
+      } else {
+        console.log('login failed');
+      }
+    }
+    xmlHttp.open("POST", rootUrl+'/roommate/login', true); // true for async
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.send(JSON.stringify({
+      username,
+      password,
+    }));
+  }
   return (
     <div className="login-center">
       <div className="login-square top_left"></div>
@@ -16,9 +47,9 @@ const Login: React.FC = () => {
           <p className="login-instructions">Sign in to find your new roommates</p>
           <hr></hr>
 
-          <input type="text" placeholder=" Username"/>
-          <input type="password" placeholder=" Password" />
-          <div className="login-login_button">
+          <input id="username" type="text" placeholder=" Username" onChange={handleUsernameChange} />
+          <input id="password" type="password" placeholder=" Password" onChange={handlePasswordChange} />
+          <div className="login-login_button" onClick={handleSubmit}>
             <p>Login</p>
           </div>
           
