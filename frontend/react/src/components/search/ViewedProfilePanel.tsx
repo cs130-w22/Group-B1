21c0fb74df7a7b25ecc545df71501b10138ca60d
Jeremy Louie <43184783+jsethlui@users.noreply.github.com>
2022-02-28 16:44:57 -0800

    UI re-design (#80)
    Co-authored-by: Andrew Chang <andrewrchang2001@gmail.com>

--- frontend/react/src/components/search/ViewedProfilePanel.tsx
+++ frontend/react/src/components/search/ViewedProfilePanel.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Roommate } from "../../util/Roommate";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import "./ViewedProfilePanel.css";
+import * as Unicons from "@iconscout/react-unicons";
 
 interface ViewedProfilePanelProps {
   roommate: Roommate | null;
 }
+
+const ICON_SIZE = 25;
 
 /**
  * Component for showing details of an individual roommate profile
@@ -19,13 +23,37 @@ export const ViewedProfilePanel: React.FC<ViewedProfilePanelProps> = (
     <div className="viewed-profile-panel">
       <Card>
         <Card.Body>
-          <Card.Title>@{roommate?.username}</Card.Title>
+          {/*profile name*/}
+          <Card.Title className="header_name">{roommate?.username}</Card.Title>
+
+          {/*wrapper for area and email*/}
+          <div className="area_email_wrapper">
+            <Card.Text className="area_email_text">
+              {/*area*/}
+              <Unicons.UilMapMarker size={ICON_SIZE} />
+              {roommate?.profile.area}
+            </Card.Text>
+
+            {/*email contact*/}
+            <Card.Text className="area_email_text email">
+              <Unicons.UilEnvelopeSend size={ICON_SIZE} />
+              <a
+                href={
+                  "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=" +
+                  roommate?.profile.email
+                }
+                target="_blank"
+                rel="noreferrer"
+              >
+                {roommate?.profile.email}
+              </a>
+            </Card.Text>
+          </div>
         </Card.Body>
-        <Card.Header>Bio: {roommate?.profile.bio}</Card.Header>
+
+        <Card.Header>{roommate?.profile.bio}</Card.Header>
         <Card.Text>
           <ListGroup>
-            <ListGroup.Item>Area: {roommate?.profile.area}</ListGroup.Item>
-            <ListGroup.Item>Email: {roommate?.profile.email}</ListGroup.Item>
             <ListGroup.Item>
               Personality: {roommate?.profile.personality.join(", ")}
             </ListGroup.Item>
